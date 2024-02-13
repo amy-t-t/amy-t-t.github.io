f4cc380617c48e784459935178d1d1c545108464
Amy <amytran3406@gmail.com>
2024-02-13 18:42:14 -0500

    Study :)

---
+++ logic.js
@@ -0,0 +1,11 @@
+// call main()
+window.onload = main;
+
+function yesButton() {
+    window.alert("good job. get your money up not your funny up.");
+}
+
+function noButton() {
+    window.alert("yo go study");
+
+}
