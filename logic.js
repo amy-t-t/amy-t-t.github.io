11a8c64593942b735fc19bdc5abfb03b15af6516
Amy <amytran3406@gmail.com>
2024-02-28 22:06:53 -0500

    hi

--- logic.js
+++ logic.js
@@ -4,14 +4,16 @@ window.onload = main;
 function yesButton() {
     const messages = ["good job :)",
                         "get your money up not your funny up >:)",
-                        "balls hehe"];
+                        "balls hehe",
+                        "keep going"];
 
     window.alert(messages[Math.floor(Math.random() * messages.length)]);
 }
 
 function noButton() {
     const messages = ["yo go study",
-                        "you're bawling, not balling"];
+                        "you're bawling, not balling",
+                        "you miss 100% of the shots you don't take"];
 
     window.alert(messages[Math.floor(Math.random() * messages.length)]);
 }
