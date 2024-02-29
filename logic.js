d5d1f01c86a4ec21369751a96cc00e2240f051d8
Amy <amytran3406@gmail.com>
2024-02-28 21:48:33 -0500

    hi

---
+++ logic.js
@@ -0,0 +1,12 @@
+// call main()
+window.onload = main;
+
+function yesButton() {
+    const messages = ["Saab", "Volvo", "BMW"];
+    window.alert("good job. get your money up not your funny up.");
+}
+
+function noButton() {
+    window.alert("yo go study");
+
+}
