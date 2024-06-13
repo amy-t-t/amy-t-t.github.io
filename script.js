4ce091f765d70b3ced001fea73cd535c4d8f2790
Amy <amytran3406@gmail.com>
2024-06-13 13:52:09 -0400

    Photography website

---
+++ script.js
@@ -0,0 +1,24 @@
+// call main()
+window.onload = main;
+
+function homePage() {
+    window.location.href = "index.html";
+}
+
+function photographyCollection() {
+    window.location.href = "photography.html";
+}
+
+
+/*
+
+function homeButton() {
+    const messages = ["good job :)",
+                        "get your money up not your funny up >:)",
+                        "balls hehe",
+                        "keep going"];
+
+    window.alert(messages[Math.floor(Math.random() * messages.length)]);
+}
+
+*/
